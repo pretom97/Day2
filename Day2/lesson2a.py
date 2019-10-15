00a35397cdaf1a76300620f06a8d23c211482553
pretom97 <56485431+pretom97@users.noreply.github.com>
2019-10-15 17:02:42 +0600

    Add files via upload

---
+++ Day2/lesson2a.py
@@ -0,0 +1,15 @@
+a, b, c = 5, 3.2, "\"hello\""
+print(a)
+print(b)
+print(c)
+
+print("%s\t%s\t%s" % (a,b,c))
+
+print("%s\n%s\n%s" % (a,b,c))
+
+print('i love {1} and {0}'.format('bread','butter'))
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+
+print(message)
---
+++ Day2/lesson2b.py
@@ -0,0 +1,16 @@
+s = "  hAve a niCe DAy"
+
+print(s)
+print(s.upper())
+print(s.lower())
+print(s.title())
+print(s.capitalize())
+print(s.swapcase())
+print(s.strip())
+print(s.center(50))
+print(s.center(50,'*'))
+print(s.replace('niCe','good'))
+print(dir(s))
+print(help(s.title))
+
+print(type(s))
